d37bb8acb667d5c94de812f3dad10c16f74973c3
MitulKirti <kirtimitul1@email.com>
2021-01-01 11:49:46 +0530

    Passing increment prop from App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       monsters: [],
       searchField: "",
+      count: 0 + this.props.increment,
     };
   }
   componentDidMount() {
@@ -17,8 +18,22 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
   handleChange = (e) => {
-    this.setState({ searchField: e.target.value })
-  }
+    this.setState(() => {
+      return {
+        searchField: e.target.value,
+      };
+    });
+  };
+  handleChangeBtn = () => {
+    this.setState(
+      (prevState, prevProps) => {
+        return {
+          count: prevState.count + prevProps.increment,
+        };
+      },
+      () => console.log(this.state.count)
+    );
+  };
 
   render() {
     const { monsters, searchField } = this.state; //destructuring concept because we do not want to modify the original state/raw state
@@ -33,6 +48,8 @@ class App extends Component {
           handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
+        {this.state.count} <br />
+        <button onClick={this.handleChangeBtn}>Update</button>
       </div>
     );
   }
